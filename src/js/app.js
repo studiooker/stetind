f7c811c5dfa4b5210496446162a0b79a663b7b4c
espenoker <espen@studio-oker.com>
2020-04-20 13:42:07 +0200

    remove bg script

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,4 @@
-import changeText from "./changetext"
 import animate from "./animate"
-import BackgroundCheck from "./bgcheck"
 
 animate()
 
-BackgroundCheck.init({
-    targets: '.target',
-    images: 'body',
-    maxDuration: 100, 
-  });
