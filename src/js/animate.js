8261c18d8b4150a09bb7ab996c443118ce20acc7
espenoker <espen@studio-oker.com>
2020-04-23 12:10:59 +0200

    responsive bg, bg scroll position

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,7 +1,7 @@
 import inView from 'in-view'
 
 export default function () {
-inView('h2')
+inView('.fade')
     .on('enter', el => {
         el.style.opacity = 1;
     })
@@ -12,8 +12,10 @@ inView('h2')
 
 const main = document.querySelector("main")
 const nav = document.querySelector("nav")
+const bgTag = document.querySelector("#bg")
+
 document.addEventListener("DOMContentLoaded", function() {
     main.style.opacity = '1';
     nav.style.opacity = '1';
-
+    bgTag.style.objectPosition = `50% 0%`
 });
--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,15 @@ import animate from "./animate"
 
 animate()
 
+const bgTag = document.querySelector("#bg")
+const bodyTag = document.querySelector("body")
+
+document.addEventListener("scroll", function(){
+    const pixels = window.pageYOffset
+    const pageHeight = bodyTag.getBoundingClientRect().height
+    const distance = pageHeight - window.innerHeight
+
+    const percentage = pixels / distance
+
+    bgTag.style.objectPosition = `50% ${100 * percentage}% `
+})
